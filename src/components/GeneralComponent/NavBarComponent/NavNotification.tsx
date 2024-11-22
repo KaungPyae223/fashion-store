b036b5f9b0b43bbee9eaa77304deb1668a818e6d
Kaung223 <kaungpyaeaung8123@gmail.com>
2024-11-22 22:45:59 +0630

    feat(ui nav): Marquee Nav Notification is finished

--- src/components/GeneralComponent/NavBarComponent/NavNotification.tsx
+++ src/components/GeneralComponent/NavBarComponent/NavNotification.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React from "react";
 import NavNotificationText from "./NavNotificationText";
-import  {motion}  from "framer-motion";
+import { motion } from "framer-motion";
 
 const NavNotification = () => {
   const NotiText: string[] = [
@@ -14,17 +15,23 @@ const NavNotification = () => {
   ];
 
   return (
-    <div className="container bg-black py-2 px-12 text-sm text-white overflow-hidden">
-      <motion.div
-        initial={{ x: "100%" }}
-        animate={{ x: "-100%" }}
-        transition={{ repeat: Infinity, duration: 10, ease: "linear" }}
-        className="flex justify-start items-center gap-6 overflow-hidden"
-      >
-        {NotiText.map((Text, i) => (
-          <NavNotificationText title={Text} key={i} />
-        ))}
-      </motion.div>
+    <div className="bg-black">
+      <div className="py-2 mx-12 text-sm text-white overflow-hidden">
+        <motion.div
+          initial={{ x: "0%" }}
+          animate={{ x: "-50%" }}
+          transition={{ repeat: Infinity, duration: 60, ease: "linear" }}
+          className="flex items-center gap-6 w-fit"
+        >
+          {NotiText.map((Text, i) => (
+            <NavNotificationText title={Text} key={i} />
+          ))}
+          {NotiText.map((Text, i) => (
+            <NavNotificationText title={Text} key={i} />
+          ))}
+        </motion.div>
+        
+      </div>
     </div>
   );
 };
